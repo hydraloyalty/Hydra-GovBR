3f321ef78b87f29a2bfb03b634ba364d54a1e560
Fênix <151000369+fenixsa@users.noreply.github.com>
2023-12-11 19:53:44 -0300

    Trocando assessoria

--- scripts.js
+++ scripts.js
@@ -253,7 +253,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.addEventListener('DOMContentLoaded', function() {
     var perfis = {
-        perfil1: { nome: "Cruz Assessoria", imagem: "perfil1.png" },
+        perfil1: { nome: "Barros Assessoria", imagem: "perfil1.png" },
         perfil2: { nome: "Filgueira Assessoria", imagem: "perfil2.png" },
         perfil3: { nome: "Banco Master", imagem: "perfil3.png" },
         perfil4: { nome: "Banco Pine", imagem: "perfil4.png" }
